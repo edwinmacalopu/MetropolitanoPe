4def3c48ad91fe04504e22d964cb9d3cc0e60564
edwinmacalopu <khiwendz@gmail.com>
2019-05-09 23:20:38 -0500

    add new interfaz Detallestaciones commit

--- lib/pages/buses.dart
+++ lib/pages/buses.dart
@@ -12,8 +12,7 @@ List<String> bustitle=["A","B","C","D","EX1","EX2","EX3","EX4","EX5","EX6","EX7"
 List<String> horarios=["Lunes a Domingos","Lunes a Domingos","Lunes a Domingos","Lunes a Viernes","Lunes a Viernes","Lunes a Viernes","Lunes a Viernes","Solo Sabados","Lunes a Viernes","Lunes a Viernes","Lunes a Viernes","Lunes a Viernes","Lunes a Viernes","Lunes a Viernes","Lunes a Viernes","","",""];
 List<String> desdea=["Naranjal - Est. Central","Naranjal - Matellini","Matellini - R. Castilla","Naranjal - Est. Central","Est. Central - Matellini","Naranjal - 28 de Julio","Naranjal - 28 de Julio","Naranjal - Plaza Flores","Naranjal - Plaza Flores","Izaguirre - Benavides","Tomás Valle - Angamos","Izaguirre - Plaza Flores","Uni - Bneavides","Naranjal - Ricardo Palma","Naranjal - Est. Central","","",""];
   @override
-  Widget build(BuildContext context) {
-      
+  Widget build(BuildContext context) {      
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: Colors.blueAccent,
@@ -119,7 +118,10 @@ return Positioned(
                               ),      
                             ),
                             onTap: (){
-                              print(Text('${bustitle[index]}'));
+                              Navigator.push(context,MaterialPageRoute(
+                                builder: (context)=>Busdetalle(detalle: bustitle[index],),
+                              ));
+                              //print(Text('${bustitle[index]}'));
                             },
                              )  ,                
                           );
@@ -131,4 +133,54 @@ return Positioned(
                 )
               );
    }
+ 
+   }
+     class Busdetalle extends StatefulWidget {
+       final String detalle;
+        Busdetalle({this.detalle});
+     @override
+     _BusdetalleState createState() => _BusdetalleState();
+   }
+   
+   class _BusdetalleState extends State<Busdetalle> {
+
+     List<String> C=["Ramon Castilla","Tacna","Jiron de la Union","Colmena","Estacion central"];
+     @override
+     Widget build(BuildContext context) {
+       return Scaffold(
+         appBar: AppBar(
+           backgroundColor: Colors.blueAccent,
+           elevation: 0,
+           title: Text(this.widget.detalle),
+         ),
+         body: Stack(
+           children: <Widget>[            
+             Container(
+               
+               height: 50,
+                color: Colors.yellow,
+             ),
+            ListView.builder(
+             itemCount: C.length,
+             itemBuilder: (context,index){                
+                return Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                   children: <Widget>[
+                     CircleAvatar(
+                       maxRadius: 5,
+                       backgroundColor: Colors.red,
+                     ),
+                     Text(C[index],style: TextStyle(fontSize: 15),),
+                   ],
+                 );
+                
+             },
+           ),
+          
+            
+           ],
+         ),
+       );
+     }
    }
