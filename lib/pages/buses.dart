ac542f412ad40080d3f8e7c74c5b0c3c52533acb
edwinmacalopu <khiwendz@gmail.com>
2019-05-23 23:34:04 -0500

    add switch ex4  commit

--- lib/pages/buses.dart
+++ lib/pages/buses.dart
@@ -8,9 +8,10 @@ class  Bus extends StatefulWidget {
 
 class _BusState extends State<Bus> {
 
-List<String> bustitle=["A","B","C","D","EX1 mañana","EX1 Tarde","EX2","EX3","EX4","EX5","EX6","EX7","EX8","EX9","Sx","SXN","","",""];
-List<String> horarios=["Lunes a Domingos","Lunes a Domingos","Lunes a Domingos","Lunes a Viernes","Lunes a Viernes","Lunes a Viernes","Lunes a Viernes","Lunes a Viernes","Solo Sabados","Lunes a Viernes","Lunes a Viernes","Lunes a Viernes","Lunes a Viernes","Lunes a Viernes","Lunes a Viernes","Lunes a Viernes","","",""];
+List<String> bustitle=["A","B","C","D","EX1","EX1","EX2","EX2","EX3","EX3","EX4","EX7","EX8","EX9","Sx","SXN","","",""];
+List<String> horarios=["Lunes a Domingos","Lunes a Domingos","Lunes a Domingos","Lunes a Viernes","Mañana","Tarde","Mañana","Tarde","Mañana - L a V","Tarde - L a V","Solo Sabados","Lunes a Viernes","Lunes a Viernes","Lunes a Viernes","Lunes a Viernes","Lunes a Viernes","","",""];
 List<String> desdea=["Naranjal - Est. Central","Naranjal - Matellini","Matellini - R. Castilla","Naranjal - Est. Central","Est. Central - Matellini","Est. Central - Matellini","Naranjal - 28 de Julio","Naranjal - 28 de Julio","Naranjal - Plaza Flores","Naranjal - Plaza Flores","Izaguirre - Benavides","Tomás Valle - Angamos","Izaguirre - Plaza Flores","Uni - Bneavides","Naranjal - Ricardo Palma","Naranjal - Est. Central","","",""];
+List<String> codbus=["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16"]; 
   @override
   Widget build(BuildContext context) {      
     double width = MediaQuery.of(context).size.width;
@@ -119,7 +120,7 @@ return Positioned(
                             ),
                             onTap: (){
                               Navigator.push(context,MaterialPageRoute(
-                                builder: (context)=>Busdetalle(detalle: bustitle[index],),
+                                builder: (context)=>Busdetalle(detalle: bustitle[index],codbus:codbus[index]),
                               ));
                               //print(Text('${bustitle[index]}'));
                             },
@@ -137,8 +138,9 @@ return Positioned(
    }
   class Busdetalle extends StatefulWidget {
        final String detalle;
+       final String codbus;
         //Busdetalle({this.detalle});
-      Busdetalle({Key key, @required this.detalle}) : super(key: key);
+      Busdetalle({Key key, @required this.detalle,@required this.codbus}) : super(key: key);
   
      @override
      _BusdetalleState createState() => _BusdetalleState();
@@ -149,25 +151,40 @@ return Positioned(
    @override
   void initState() { 
     super.initState();
-    final String letrabus=this.widget.detalle;
+    final String letrabus=this.widget.codbus;
     switch(letrabus) {
-       case "A":
+       case "1":
        detallebus=["Narananjal","Izaguirre","Pacífico","Independencia","Los Jasminez","Tomás Valle","El Milagro","Honorio Delgado","UNI","Parque del Trabajo","Caquetá","Castilla","Tacna","Jr de la Unión","Colmena","Est. Central"];
        break;
-         case "B":
+         case "2":
          detallebus=["Naranjal","Izaguirre","Pacífico","Independencia","Los Jasminez","Tomás Valle","El Milagro","Honorio Delgado","UNI","Parque del Trabajo","Caqueta","Dos de Mayo","Quilca","España","Est. Central","México","Canadá","Javier Prado","Canaval Moreyra","Aramburú","Domingo Orué","Angamos","Ricardo Palma","Benavides","28 de Julio","Plaza de Flores","Balta","Bulevar","Estadio Unión","Escuela Militar","Terán","Rosario de Villa","Matellini"];
            break;
-         case "C":
+         case "3":
          detallebus=["Ramon Castilla","Tacna","Jiron de la Union","Colmena","Estacion Central","Estadio Nacional","México","Canada","Javier Prado","Canaval y Moreyra","Aramburú","Domingo Orué","Angamos","Ricardo Palma","Benavides","28 de Julio","Plaza de Flores","Balta","Bulevar","Estadio Unión","Escuela Militar","Terán","Rosario de Villa","Matellini"];
          break;
-          case "D":
+          case "4":
          detallebus=["Naranjal","Izaguirre","Pacífico","Independencia","Los Jazmines","Tomás Valle","El Milagro","Honorio Delgado","UNI","Parque del Trabajo","Caqueta","Dos de Mayo","Quilca","España","Est. Central"];
          break;
-         case "EX1 mañana":
+         case "5":
          detallebus=["Est. Central","Estadio Nacional","Javier Prado","Canaval y Moreyra","Angamos","Ricardo Palma","28 de Julio","Balta","Bulevar","Estadio Unión","Escuela Militar","Terán","Rosario de Villa","Matellini"];
          break;
-         case "EX1 Tarde":
+         case "6":
          detallebus=["Est. Central","Estadio Nacional","Javier Prado","Canaval y Moreyra","Angamos","Ricardo Palma","28 de Julio","Balta","Bulevar","Estadio Unión","Escuela Militar","Terán","Rosario de Villa","Matellini"];
+          break;
+          case "7":
+         detallebus=["Naranjal","Canadá","Javier Prado","Ricardo Palma","28 de Julio"];
+          break;
+          case "8":
+         detallebus=["solo de sur a norte"];
+          break;
+           case "9":
+         detallebus=["Naranjal","Angamos","Benavides","28 de Julio"];
+          break;
+           case "10":
+         detallebus=["solo de sur a norte"];
+          break;
+             case "11":
+         detallebus=["Naranjal","Izaguirre","Tomás Valle","UNI","Caquetá","2 de Mayo","España","Est. Central","Javier Prado","Canaval y Moreyra","Angamos","Plaza Flores"];
           break;
        default:
        detallebus=[""];
@@ -178,53 +195,83 @@ String textobotton="Norte a Sur";
     setState(() {
      if (textobotton=="Norte a Sur"){
           textobotton="Sur a Norte";
-           final String letrabusur=this.widget.detalle;
+           final String letrabusur=this.widget.codbus;
     switch(letrabusur) {
-       case "A":
+       case "1":
        detallebus=["Est. Central","Colmena","Jr de la Unión","Tacna","Castilla","Caquetá","Parque del Trabajo","UNI","Honorio Delgado","El Milagro","Tomás Valle","Los Jasminez","Independencia","Pacífico","Izaguirre","Narananjal"];
        break;
-         case "B":
+         case "2":
          detallebus=["Matellini","Rosario de Villa","Terán","Escuela Militar","Estadio Unión","Bulevar","Balta","Plaza de Flores","28 de Julio","Benavides","Ricardo Palma","Angamos","Domingo Orué","Aramburú","Canaval Moreyra","Javier Prado","Canadá","México","Estadio Nacional","Est. Central","España","Quilca","Dos de Mayo","Caqueta","Parque del Trabajo","UNI","Honorio Delgado","El Milagro","Tomás Valle","Los Jasminez","Independencia","Pacífico","Izaguirre","Naranjal"];
            break;
-         case "C":
+         case "3":
          detallebus=["Matellini","Rosario de Villa","Terán","Escuela Militar","Estadio Unión","Bulevar","Balta","Plaza de Flores","28 de Julio","Benavides","Ricardo Palma","Angamos","Domingo Orué","Aramburú","Canaval Moreyra","Javier Prado","Canadá","México","Estadio Nacional","Est. Central","Colmena","Jirón de la Unión","Tacna","Ramón Castilla"];
          break;
-          case "D":
+          case "4":
          detallebus=["Est. Central","España","Quilca","2 de Mayo","Caquetá","Parque del Trabajo","UNI","Honorio Delgado","El Milagro","Tomás Valle","Los Jazmines","Independencia","Pacífico","Izaguirre","Naranjal"];
          break;
-         case "EX1 mañana":
+         case "5":
          detallebus=["Matellini","Rosario de Villa","Terán","Escuela Militar","Estadio Unión","Bulevar","Balta","28 de Julio","Angamos","Canaval y Moreyna","Javier Prado","Estadio Nacional","Est. Central"];
          break;
-         case "EX1 Tarde":
+         case "6":
          detallebus=["Matellini","Rosario de Villa","Terán","Escuela Militar","Estadio Unión","Bulevar","Balta","28 de Julio","Ricardo Palma","Angamos","Javier Prado","Estadio Nacional","Est. Central"];
           break;
+            case "7":
+         detallebus=["Plaza Flores","Benavides","Angamos","Canaval y Moreyra","Javier Prado","Est. Central","España","Caqueta","UNI","Tomás Valle","Izaguirre","Naranjal"];
+          break;
+          case "8":
+         detallebus=["Ricardo Palma","Javier Prado","Canadá","Naranjal"];
+          break;
+          case "9":
+         detallebus=["solo de norte a sur"];
+          break;
+            case "10":
+         detallebus=["benavides","Angamos","Naranjal"];
+          break;
+          case "11":
+         detallebus=["Plaza Flores","Angamos","Canaval y Moreyra","Javier Prado","Est. Central","España","2 de mayo","Caquetá","UNI","Tomás Valle","Izaguirre","Naranjal"];
+          break;
+          
 
        default:
        detallebus=[""];
      }
      }else{
        textobotton="Norte a Sur";
-        final String letrabusurs=this.widget.detalle;
+        final String letrabusurs=this.widget.codbus;
        switch(letrabusurs) {
-       case "A":
+       case "1":
        detallebus=["Narananjal","Izaguirre","Pacífico","Independencia","Los Jasminez","Tomás Valle","El Milagro","Honorio Delgado","UNI","Parque del Trabajo","Caquetá","Castilla","Tacna","Jr de la Unión","Colmena","Est. Central"];
        break;
-         case "B":
+         case "2":
          detallebus=["Naranjal","Izaguirre","Pacífico","Independencia","Los Jasminez","Tomás Valle","El Milagro","Honorio Delgado","UNI","Parque del Trabajo","Caqueta","Dos de Mayo","Quilca","España","Est. Central","México","Canadá","Javier Prado","Canaval Moreyra","Aramburú","Domingo Orué","Angamos","Ricardo Palma","Benavides","28 de Julio","Plaza de Flores","Balta","Bulevar","Estadio Unión","Escuela Militar","Terán","Rosario de Villa","Matellini"];
            break;
-         case "C":
+         case "3":
          detallebus=["Ramon Castilla","Tacna","Jiron de la Union","Colmena","Estacion Central","Estadio Nacional","México","Canada","Javier Prado","Canaval y Moreyra","Aramburú","Domingo Orué","Angamos","Ricardo Palma","Benavides","28 de Julio","Plaza de Flores","Balta","Bulevar","Estadio Unión","Escuela Militar","Terán","Rosario de Villa","Matellini"];
          break;
-          case "D":
+          case "4":
          detallebus=["Naranjal","Izaguirre","Pacífico","Independencia","Los Jazmines","Tomás Valle","El Milagro","Honorio Delgado","UNI","Parque del Trabajo","Caqueta","Dos de Mayo","Quilca","España","Est. Central"];
          break;
-         case "EX1 mañana":
+         case "5":
          detallebus=["Est. Central","Estadio Nacional","Javier Prado","Canaval Moreyra","Angamos","Ricardo Palma","28 de Julio","Balta","Bulevar","Estadio Unión","Escuela Militar","Terán","Rosario de Villa","Matellini"];
          break;
-         case "EX1 Tarde":
+         case "6":
          detallebus=["Est. Central","Estadio Nacional","Javier Prado","Canaval y Moreyra","Angamos","Ricardo Palma","28 de Julio","Balta","Bulevar","Estadio Unión","Escuela Militar","Terán","Rosario de Villa","Matellini"];
           break;
-
+            case "7":
+         detallebus=["Naranjal","Canadá","Javier Prado","Ricardo Palma","28 de Julio"];
+          break;
+          case "8":
+         detallebus=["solo de sur a norte"];
+          break;
+          case "9":
+         detallebus=["Naranjal","Angamos","Benavides","28 de Julio"];
+          break;
+           case "10":
+          detallebus=["solo de sur a norte"];
+          break;
+            case "11":
+         detallebus=["Naranjal","Izaguirre","Tomás Valle","UNI","Caquetá","2 de Mayo","España","Est. Central","Javier Prado","Canaval y Moreyra","Angamos","Plaza Flores"];
+          break;
        default:
        detallebus=[""];
      }
@@ -293,7 +340,7 @@ String textobotton="Norte a Sur";
                     ),
                     CircleAvatar(
                       backgroundColor: Colors.blueAccent,
-                      radius: 7,
+                      radius:7,
                     ),
                     SizedBox(
                       width: 10,
